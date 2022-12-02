19beb27c976b74621cd1c73bf5d0c9461fc52893
Yingjia Liu <yingjia@arternal.com>
2022-12-02 11:14:29 -0500

    Export auction icon + sort alphabetical

--- src/icons/index.js
+++ src/icons/index.js
@@ -1,25 +1,27 @@
-import DashboardIcon from './DashboardIcon.svg';
-import TransactionIcon from './TransactionIcon.svg';
-import InventoryIcon from './InventoryIcon.svg';
-import ContactsIcon from './ContactsIcon.svg';
-import DealsIcon from './DealsIcon.svg';
-import InvoicesIcon from './InvoicesIcon.svg';
-import ReportsIcon from './ReportsIcon.svg';
-import SmartMailIcon from './SmartMailIcon.svg';
-import SettingsIcon from './DealsIcon.svg';
-import ArternalIcon from './ArternalIcon.svg';
-import PaymentsIcon from './PaymentsIcon.svg';
+import ArternalIcon from "./ArternalIcon.svg";
+import AuctionIcon from "./AuctionIcon.svg";
+import ContactsIcon from "./ContactsIcon.svg";
+import DashboardIcon from "./DashboardIcon.svg";
+import DealsIcon from "./DealsIcon.svg";
+import InventoryIcon from "./InventoryIcon.svg";
+import InvoicesIcon from "./InvoicesIcon.svg";
+import PaymentsIcon from "./PaymentsIcon.svg";
+import ReportsIcon from "./ReportsIcon.svg";
+import SettingsIcon from "./DealsIcon.svg";
+import SmartMailIcon from "./SmartMailIcon.svg";
+import TransactionIcon from "./TransactionIcon.svg";
 
 export {
-  DashboardIcon,
-  TransactionIcon,
-  InventoryIcon,
-  ContactsIcon,
-  DealsIcon,
-  InvoicesIcon,
-  ReportsIcon,
-  SmartMailIcon,
-  SettingsIcon,
   ArternalIcon,
+  AuctionIcon,
+  ContactsIcon,
+  DashboardIcon,
+  DealsIcon,
+  InventoryIcon,
+  InvoicesIcon,
   PaymentsIcon,
+  ReportsIcon,
+  SettingsIcon,
+  SmartMailIcon,
+  TransactionIcon,
 };
