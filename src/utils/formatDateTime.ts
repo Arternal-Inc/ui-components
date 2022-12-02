ce7d5de915b0bd5e5d64015c99fe7ae8fa1a3870
Yingjia Liu <yingjia@arternal.com>
2022-12-02 13:31:44 -0500

    Time zone support

--- src/utils/formatDateTime.ts
+++ src/utils/formatDateTime.ts
@@ -1,10 +1,23 @@
-export default (date: Date, locale = 'en-US') =>
-  date && date.toString() !== 'Invalid Date'
+export default (
+  date: Date,
+  locale = "en-US",
+  timeZone?: string,
+  timeZoneName?:
+    | "short"
+    | "long"
+    | "shortOffset"
+    | "longOffset"
+    | "shortGeneric"
+    | "longGeneric"
+) =>
+  date && date.toString() !== "Invalid Date"
     ? date.toLocaleDateString(locale, {
-        month: 'short',
-        day: 'numeric',
-        year: 'numeric',
-        hour: 'numeric',
-        minute: 'numeric',
+        month: "short",
+        day: "numeric",
+        year: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+        timeZone,
+        timeZoneName,
       })
-    : ''
+    : "";
