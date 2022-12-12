8bbfb7ce5c1f5881b9a160e37b7715bc29ffc75d
Yingjia Liu <yingjia@arternal.com>
2022-12-12 17:00:15 -0500

    Remove code causing export to fail

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,21 +1,4 @@
-declare module 'arternal-ui';
-declare module 'arternal-ui/src/icons';
-declare module 'arternal-ui/src/components';
-declare module 'arternal-ui/src/utils';
-
-export type Size = 'sm' | 'md' | 'lg'
-
-export type Variant =
-  | 'primary'
-  | 'secondary'
-  | 'info'
-  | 'success'
-  | 'warning'
-  | 'danger'
-  | 'classic'
-
-
-export type VueClassAttr =
-  | string
-  | Record<string, boolean>
-  | (string | Record<string, boolean>)[]
+declare module "arternal-ui";
+declare module "arternal-ui/src/icons";
+declare module "arternal-ui/src/components";
+declare module "arternal-ui/src/utils";
