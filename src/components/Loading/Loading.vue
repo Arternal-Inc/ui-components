008b745b19504b06f128e72f740bed434a20f9a9
Yingjia Liu <yingjia@arternal.com>
2022-12-15 13:10:49 -0500

    Fix loading spinner

--- src/components/Loading/Loading.vue
+++ src/components/Loading/Loading.vue
@@ -1,24 +1,26 @@
 <template>
   <span class="inline-flex items-center gap-2">
     <svg
-      class="inline-block h-5 w-5 animate-spin"
+      class="ml-2 w-6"
+      version="1.1"
       xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
+      xmlns:xlink="http://www.w3.org/1999/xlink"
+      viewBox="27 27 46 46"
     >
-      <circle
-        class="opacity-25"
-        cx="12"
-        cy="12"
-        r="10"
-        stroke="currentColor"
-        stroke-width="4"
-      ></circle>
       <path
-        class="opacity-75"
-        fill="currentColor"
-        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-      ></path>
+        fill="#fff"
+        d="M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50"
+      >
+        <animateTransform
+          attributeName="transform"
+          attributeType="XML"
+          type="rotate"
+          dur="1s"
+          from="0 50 50"
+          to="360 50 50"
+          repeatCount="indefinite"
+        />
+      </path>
     </svg>
 
     <slot v-if="$slots.default" />
