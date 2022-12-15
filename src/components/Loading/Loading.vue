27114a046f4ee81111438b965cada94af028d1c2
Yingjia Liu <yingjia@arternal.com>
2022-12-15 13:12:29 -0500

    Match spinner colour to text

--- src/components/Loading/Loading.vue
+++ src/components/Loading/Loading.vue
@@ -8,7 +8,7 @@
       viewBox="27 27 46 46"
     >
       <path
-        fill="#fff"
+        fill="currentColor"
         d="M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50"
       >
         <animateTransform
