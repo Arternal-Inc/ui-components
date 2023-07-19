d92b5dd5553d76a0f069254a26c7748cfe2fd125
Yingjia Liu <yingjia@arternal.com>
2023-07-19 14:03:31 -0400

    Add infinite scrolling container from CRM

---
+++ src/components/InfiniteScrollContainer/InfiniteScrollingContainer.vue
@@ -0,0 +1,117 @@
+<script lang="coffee">
+  LoadMoreWidget = require '@/components/inventory/LoadMoreWidget'
+  VirtualList = require 'vue-virtual-scroll-list'
+
+  exports.default =
+    name: 'InfiniteScrollingContainer'
+    components: { LoadMoreWidget, VirtualList }
+    props:
+      externalScroll:
+        type: Number
+        default: null
+      syncOffset:
+        type: Number
+        default: 200
+      view:
+        type: String
+        default: 'list'
+      rowHeight:
+        type: Number
+        default: 30
+      count:
+        type: Number
+        default: 0
+      available:
+        type: Number
+        default: 0
+      syncing:
+        type: Boolean
+        default: false
+      noLoading:
+        type: Boolean
+        default: false
+      hideEmptyState:
+        type: [Boolean, String]
+        default: null
+      novirtualList:
+        type: String
+        default: null
+      maxHeightVirtualList:
+        type: String
+        default: null
+
+    data: ->
+      viewportHeight: 0
+      viewportWidth: 0
+      scroll: 0
+
+    mounted: ->
+      @viewportWidth = @$refs.container?.clientWidth
+      @viewportHeight = @$refs.container?.clientHeight
+
+    computed:
+      moreItems: -> @available > @count and !@noLoading
+      target: -> (@rowHeight * @count) - (@viewportHeight + @syncOffset)
+      numShownItems: -> if @viewportHeight then Math.round @viewportHeight / @rowHeight else 9
+
+    watch:
+      scroll: (scroll) ->
+        return if @syncing or not @moreItems
+        if scroll > @target
+          @loadMore()
+
+      syncing: (syncing) ->
+        return if syncing or not @moreItems
+        if @scroll > @target
+          @loadMore()
+
+      externalScroll: (scroll) -> @setScroll { srcElement: { scrollTop: scroll }}
+
+    methods:
+      setScroll: (event) -> @scroll = event?.srcElement?.scrollTop ? 0
+      loadMore: -> @$emit 'loadMore' unless @noLoading
+</script>
+
+<template lang="pug">
+  .InfiniteScrollingContainer(ref = 'container')
+    virtual-list.virtualList(
+      v-if = '!novirtualList'
+      :size = 'rowHeight'
+      :remain = 'numShownItems'
+      :onscroll = 'setScroll'
+      :class = '{ maxHeight: maxHeightVirtualList }'
+      )
+      slot
+      load-more-widget(
+        :hideEmptyState = 'hideEmptyState'
+        :view = 'view'
+        :syncing = 'syncing'
+        :hasMore = 'moreItems'
+        @load = 'loadMore'
+        )
+    .noVirtualList(v-else)
+      slot
+      load-more-widget(
+        :hideEmptyState = 'hideEmptyState'
+        :view = 'view'
+        :syncing = 'syncing'
+        :hasMore = 'moreItems'
+        @load = 'loadMore'
+        )
+</template>
+
+<style lang="stylus" scoped>
+  @import '~assets/styles/theme'
+
+  .InfiniteScrollingContainer
+    width 100%
+    height 100%
+    overflow-y auto
+    padding-bottom 60px
+
+    .virtualList
+      overflow-x hidden
+
+      &.maxHeight
+        height 100% !important
+</style>
---
+++ src/components/InfiniteScrollContainer/index.ts
@@ -0,0 +1,1 @@
+export { default as InfiniteScrollingContainer } from './InfiniteScrollingContainer.vue'
