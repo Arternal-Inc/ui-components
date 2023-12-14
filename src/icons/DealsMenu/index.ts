46508e4c13458ced6db3e2c7c3b3fa9a903effa8
Jayvin Chang <jayvin@arternal.com>
2023-12-14 16:44:09 -0500

    fix deals icons export path

--- src/icons/DealsMenu/index.ts
+++ src/icons/DealsMenu/index.ts
@@ -1,7 +1,7 @@
-import DealsBuySideIcon from 'DealsBuySideIcon.svg'
-import DealsFixedFeeIcon from 'DealsFixedFeeIcon.svg'
-import DealsPipelineViewIcon from 'DealsPipelineViewIcon.svg'
-import DealsSaleSideIcon from 'DealsSaleSideIcon.svg'
+import DealsBuySideIcon from './DealsBuySideIcon.svg'
+import DealsFixedFeeIcon from './DealsFixedFeeIcon.svg'
+import DealsPipelineViewIcon from './DealsPipelineViewIcon.svg'
+import DealsSaleSideIcon from './DealsSaleSideIcon.svg'
 
 export {
   DealsBuySideIcon,
